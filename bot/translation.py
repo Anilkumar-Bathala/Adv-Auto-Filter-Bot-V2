e820d5cc27afbbeba0a331e28e82bb1d1f1c8c07
Anil Kumar <43469041+Anilkumar-Bathala@users.noreply.github.com>
2021-09-07 10:09:59 -0700

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
-
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+I was Developed for @upto_telugu_movies 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@upto_telugu_movies
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +44,9 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/DarlingAnil">Anil</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
