17fef26b21cb2a2aa9a9bef85edd9d79ffafb3cf
Anil Kumar <43469041+Anilkumar-Bathala@users.noreply.github.com>
2021-09-07 10:53:39 -0700

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -49,4 +49,6 @@ For More Details Click Help Button Below..
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://github.com/Anilkumar-Bathala/Adv-Auto-Filter-Bot-V2.git">Click Me</a></i>
 """
