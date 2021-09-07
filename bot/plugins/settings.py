15209c14efd7a0954b699f68ac42dcee709af2ea
Anil Kumar <43469041+Anilkumar-Bathala@users.noreply.github.com>
2021-09-07 10:20:59 -0700

    Update settings.py

--- bot/plugins/settings.py
+++ bot/plugins/settings.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
-
 import re
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
